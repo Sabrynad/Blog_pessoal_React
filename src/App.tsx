c341f75f4c9e19ac426000c3d7df7781ae421c50
Sabryna <sabrynadeangelo@gmail.com>
2022-09-07 10:34:02 -0300

    criando o Blog pessoal e o componente Home no React

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Grid} from "@material-ui/core";
+import Home from './Page/Home/Home';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Grid container spacing={3}>
+      <Grid item xs={12} sm={12}>
+       <Home />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+       <Home />
+      </Grid>
+      <Grid item xs={12} sm={6}>
+       <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+       <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+       <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+       <Home />
+      </Grid>
+      <Grid item xs={12} sm={3}>
+       <Home />
+      </Grid>
+    </Grid>
+    </>
   );
 }
 
---
+++ src/Page/Home/Home.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {Box, Paper} from "@material-ui/core";
+import "./Home.css";
+
+function Home(){
+   return(
+    <>
+      <Paper>
+        <Box>
+            <Box>
+              <h1>Titulo</h1>
+            </Box>
+            <img src="https://i.pinimg.com/originals/f5/f9/f3/f5f9f3c041de6d0cdef88cdb54c2fc2c.jpg"alt="" />
+            <Box>
+              <button>
+                
+              </button>
+              <button>
+
+              </button>
+            </Box>
+        </Box>
+    </Paper>
+         
+    </>
+   );
+}
+
+export default Home;
